66f29395e64450f6dffd230c22b1603a631a85eb
Aryee Allen Nii Ayitey <82196885+PAPA-allen@users.noreply.github.com>
2025-07-21 10:43:07 +0000

    fixed linked in

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -3,19 +3,19 @@ import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import Link from 'next/link';
 
 const socials = [
-    { icon: <FaGithub />, path: "https://github.com/PAPA-allen", color: "bg-black", borderColor: "border-gray-700" },
-    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/allen-nii-ayitey-aryee-a45093169/", color: "bg-blue-700", borderColor: "border-blue-600" },
-    { icon: <FaTwitter />, path: "https://x.com/allen_aryee", color: "bg-blue-400", borderColor: "border-blue-300" },
+  { icon: <FaGithub />, path: "https://github.com/PAPA-allen", color: "bg-black", borderColor: "border-gray-700" },
+  { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/allen-aryee-a45093169/", color: "bg-blue-700", borderColor: "border-blue-600" },
+  { icon: <FaTwitter />, path: "https://x.com/allen_aryee", color: "bg-blue-400", borderColor: "border-blue-300" },
 ];
 
 const Socials = () => {
   return (
     <div className="flex justify-center gap-6 p-4">
       {socials.map((social, index) => (
-        <Link 
-          key={index} 
-          href={social.path} 
-          passHref 
+        <Link
+          key={index}
+          href={social.path}
+          passHref
           className={`flex justify-center items-center w-12 h-12 rounded-full border-2 ${social.color} ${social.borderColor} text-white transition-transform transform hover:scale-110`}
         >
           {social.icon}
